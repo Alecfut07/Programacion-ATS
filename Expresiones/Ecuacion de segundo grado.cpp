b501062e45ec7ced4a0a91a743e811b81b638257
Alecfut07 <ortega.cisneros.alec@gmail.com>
2019-10-27 19:12:29 -0700

    Ejercicio

---
+++ Expresiones/Ecuacion de segundo grado.cpp
@@ -0,0 +1,28 @@
+/*Escriba un programa que calcule las soluciones de una ecuación
+de segundo grado de la forma ax^2 + bx + c = 0, teniendo en cuenta
+que: x = -b +- sqrt(pow(b, 2) - 4ac) / 2a*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	float a, b, c, x1 = 0, x2 =0;
+	
+	cout << "Digite el valor de a: ";
+	cin >> a;
+	cout << "Digite el valor de b: ";
+	cin >> b;
+	cout << "Digite el valor de c: ";
+	cin >> c;
+	
+	x1 = (-b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
+	x2 = (-b - sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
+	
+	cout << "\nEl resultado de x1 es: " << x1 << endl;
+	cout << "El resultado de x2 es: " << x2 << endl;
+	
+	return 0; 
+}
