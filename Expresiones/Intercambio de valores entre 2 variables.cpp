c90b119a09fbcc6e327e36e325355188c4196e72
Alecfut07 <ortega.cisneros.alec@gmail.com>
2019-10-27 18:30:48 -0700

    Practica

---
+++ Expresiones/Intercambio de valores entre 2 variables.cpp
@@ -0,0 +1,35 @@
+/*Escriba un fragmento de programa que intercambie los valores de dos 
+variables.*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x, y, aux;
+	
+	cout << "Digite el valor de x: ";
+	cin >> x;
+	cout << "Digite el valor de y: ";
+	cin >> y;
+	
+	/*
+	
+	y = 5
+	
+	aux = 10
+	x = 5
+	y = 10
+	 
+	*/
+	
+	aux = x;
+	x = y;
+	y = aux;
+	
+	cout << "\nEl nuevo valor de x es: " << x << endl;
+	cout << "El nuevo valor de y es: " << y << endl;
+	
+	return 0;
+}
