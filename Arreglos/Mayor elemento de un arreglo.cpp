3cdb63fd7a339c84d81f459b756f71461e3d14a6
Alecfut07 <ortega.cisneros.alec@gmail.com>
2019-11-03 11:25:08 -0800

    Practica

---
+++ Arreglos/Mayor elemento de un arreglo.cpp
@@ -0,0 +1,32 @@
+/*Desarrolle un programa que lea de la entrada estándar un vector de enteros
+y determine el mayor elemento del vector.*/
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	int numeros[100], n, mayor = 0;
+	
+	cout << "Digite el numero de elementos del arreglo: ";
+	cin >> n;
+	
+	for (int i = 0; i < n; i++)
+	{
+		cout << i + 1 << ". Digite un numero: "; //1. Digite un numero: 
+		cin >> numeros[i];
+		
+		//Ejemplo de Arreglo: 1, 2, 4, 10, 5
+		if (numeros[i] > mayor)
+		{
+			mayor = numeros[i]; //10
+		}
+	}
+	
+	cout << "\nEl mayor elemento del vector es: " << mayor << endl;
+	
+	system("pause");
+	return 0;
+}
