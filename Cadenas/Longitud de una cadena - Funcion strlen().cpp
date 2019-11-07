2753fc09db30d4bb089f2f97869c6b626157e28c
Alecfut07 <ortega.cisneros.alec@gmail.com>
2019-11-07 13:06:57 -0800

    Practica

---
+++ Cadenas/Longitud de una cadena - Funcion strlen().cpp
@@ -0,0 +1,20 @@
+//Longitud de unas cadenas de caracteres - Función strlen()
+
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+	char palabra[] = "hola que tal?";
+	int longitud = 0;
+	
+	longitud = strlen(palabra);
+	
+	cout << "Numero de elementos de la cadena es: " << longitud << endl;
+	
+	system("pause");
+	return 0;
+}
