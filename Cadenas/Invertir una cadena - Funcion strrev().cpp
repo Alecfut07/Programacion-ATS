dbd4d7570a9b7b34abf7a885b57ac1ee385ef63b
Alecfut07 <ortega.cisneros.alec@gmail.com>
2019-11-09 18:24:45 -0800

    Practica

---
+++ Cadenas/Invertir una cadena - Funcion strrev().cpp
@@ -0,0 +1,19 @@
+//Invertir una cadena - Función strrev()
+
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+	char cad[] = "Alec";
+	
+	strrev(cad);
+	
+	cout << cad << endl;
+	
+	system("pause");
+	return 0;
+}
