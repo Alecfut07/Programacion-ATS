590143398bc25da2202cb7a9fdfe37b1f250644b
Alecfut07 <ortega.cisneros.alec@gmail.com>
2019-12-23 19:28:34 -0800

    Practica

---
+++ Punteros/Punteros a Estructura.cpp
@@ -0,0 +1,39 @@
+//Punteros a Estructuras
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+struct Persona
+{
+	char nombre[30];
+	int edad;
+} persona, *puntero_persona = &persona;
+
+//Prototipo de Función.
+void pedirDatos();
+void mostrarDatos(Persona *);
+
+int main()
+{
+	pedirDatos();
+	mostrarDatos(puntero_persona);
+	
+	system("pause");
+	return 0;
+}
+
+void pedirDatos()
+{
+	cout << "Digite su nombre: ";
+	cin.getline(puntero_persona->nombre, 30, '\n');
+	cout << "Digite su edad: ";
+	cin >> puntero_persona->edad;
+}
+
+void mostrarDatos(Persona *puntero_persona)
+{
+	cout << "\nSu nombre es: " << puntero_persona->nombre << endl;
+	cout << "Su edad es: " << puntero_persona->edad << endl;
+}
